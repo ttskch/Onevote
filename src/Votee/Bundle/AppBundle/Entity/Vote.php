97e7b86e68a3ba072975068d14f51a4a82ffc9fb
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2015-03-17 16:27:58 +0900

    add Vote entity

---
+++ src/Votee/Bundle/AppBundle/Entity/Vote.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Votee\Bundle\AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Vote
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Votee\Bundle\AppBundle\Entity\VoteRepository")
+ */
+class Vote
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="voter", type="string", length=255)
+     */
+    private $voter;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="note", type="text")
+     */
+    private $note;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set voter
+     *
+     * @param string $voter
+     * @return Vote
+     */
+    public function setVoter($voter)
+    {
+        $this->voter = $voter;
+
+        return $this;
+    }
+
+    /**
+     * Get voter
+     *
+     * @return string 
+     */
+    public function getVoter()
+    {
+        return $this->voter;
+    }
+
+    /**
+     * Set note
+     *
+     * @param string $note
+     * @return Vote
+     */
+    public function setNote($note)
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    /**
+     * Get note
+     *
+     * @return string 
+     */
+    public function getNote()
+    {
+        return $this->note;
+    }
+}
