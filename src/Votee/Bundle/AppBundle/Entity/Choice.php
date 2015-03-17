58f3756dd88aa634f19caab20cc6c34aa641a9d6
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2015-03-17 16:27:28 +0900

    add Choice entity

---
+++ src/Votee/Bundle/AppBundle/Entity/Choice.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Votee\Bundle\AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Choice
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Votee\Bundle\AppBundle\Entity\ChoiceRepository")
+ */
+class Choice
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="note", type="text")
+     */
+    private $note;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Choice
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set note
+     *
+     * @param string $note
+     * @return Choice
+     */
+    public function setNote($note)
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    /**
+     * Get note
+     *
+     * @return string 
+     */
+    public function getNote()
+    {
+        return $this->note;
+    }
+}
